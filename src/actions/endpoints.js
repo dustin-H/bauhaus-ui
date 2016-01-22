2f43ae9a3455ad11fa8c633852c966ba8b1ed709
dustin-H <dustin.hoffner@gmail.com>
2016-01-22 18:28:49 +0100

    removed outdated stuff

--- src/actions/endpoints.js
+++
@@ -1,9 +0,0 @@
-import languagePlugin from '../utils/i18n/plugin.js';
-import * as types from '../constants/ActionTypes.js';
-
-export function setEndpoints(endpoints) {
-	return {
-		type: types.SET_ENDPOINTS,
-		endpoints
-	};
-}
