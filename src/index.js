d3c61d219bc53415311bac9bdb07e2a032c57242
dustin-H <dustin.hoffner@gmail.com>
2016-01-22 18:31:54 +0100

    register modules for module(component)-loader

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 
+import registerModules from './modules/registerModules.js';
+registerModules();
+
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
