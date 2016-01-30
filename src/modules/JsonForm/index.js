76dbad41b316fc7a2652d2306309b7ace2891dfe
dustin-H <dustin.hoffner@gmail.com>
2016-01-30 16:05:41 +0100

    Get, Save and Delete working for JsonForm. Closes #1

--- src/modules/JsonForm/index.js
+++ src/modules/JsonForm/index.js
@@ -7,7 +7,7 @@ import superagent from 'superagent';
 import superagentPlugin from '../../utils/helpers/superagentPlugin.js';
 
 class JsonForm extends Component {
-	constructor() {
+	/*constructor() {
 		super();
 		this.state = {
 			savedData: {},
@@ -17,61 +17,127 @@ class JsonForm extends Component {
 			changed: false,
 			error: false
 		}
-	}
+	}*/
 	getValue(path) {
-		return objectPath.get(this.state.data, path);
+		const {bauhaus} = this.props;
+		return objectPath.get(bauhaus._state.data, path);
 	}
 	setValue(path, value) {
-		var state = Object.assign({}, this.state);
+		const {bauhaus} = this.props;
+		var state = Object.assign({}, bauhaus._state);
 		if (state.initialLoaded === true && state.loading === false) {
 			var data = state.data;
 			objectPath.set(data, path, value);
 			state.data = data;
-			state.changed = JSON.stringify(state.data) === JSON.stringify(state.savedData);
+			state.changed = (JSON.stringify(state.data) !== state.savedData);
 		}
-		this.setState(state);
+		bauhaus._setState(state);
 	}
-	componentDidMount() {
-		this.state.loading = true;
-		this.setState(this.state);
+	loadData() {
+		const {bauhaus} = this.props;
+      var state = Object.assign({}, bauhaus._state);
+		state.loading = true;
+		bauhaus._setState(state);
 		superagent
-			.get(this.props.bauhaus.props.getUrl)
+			.get(this.props.bauhaus.props.url)
 			.accept('json')
 			.use(superagentPlugin())
 			.end((function(err, res) {
+            var state = Object.assign({}, bauhaus._state);
 				if (err != null || res == null || res.body == null || res.body.jsondata == null) {
-					this.state.error = true;
-					return this.setState(this.state);
+					state.error = true;
+					return bauhaus._setState(state);
 				}
-				this.state.data = res.body.jsondata;
-				this.state.savedData = res.body.jsondata;
-				this.state.initialLoaded = true;
-				this.state.loading = false;
-				this.setState(this.state);
+				state.data = res.body.jsondata;
+				state.savedData = JSON.stringify(res.body.jsondata);
+				state.initialLoaded = true;
+				state.loading = false;
+				state.changed = false;
+				bauhaus._setState(state);
 			}).bind(this));
+	}
+	saveData() {
+		const {bauhaus} = this.props;
+      var state = Object.assign({}, bauhaus._state);
+		bauhaus._state.loading = true;
+		bauhaus._setState(bauhaus._state);
+		superagent
+			.put(bauhaus.props.url)
+         .send(bauhaus._state.data)
+			.accept('json')
+			.use(superagentPlugin())
+			.end((function(err, res) {
+				if (err != null) {
+               var state = Object.assign({}, bauhaus._state);
+					bauhaus._state.error = true;
+					return bauhaus._setState(bauhaus._state);
+				}
+				this.loadData();
+			}).bind(this));
+	}
+	delete() {
+		const {bauhaus} = this.props;
+      var state = Object.assign({}, bauhaus._state);
+		bauhaus._state.loading = true;
+		bauhaus._setState(bauhaus._state);
+		superagent
+			.delete(bauhaus.props.url)
+			.accept('json')
+			.use(superagentPlugin())
+			.end((function(err, res) {
+				if (err != null) {
+               var state = Object.assign({}, bauhaus._state);
+					bauhaus._state.error = true;
+					return bauhaus._setState(bauhaus._state);
+				}
+				this.loadData();
+			}).bind(this));
+	}
+	componentWillMount() {
+		const {bauhaus} = this.props;
+		bauhaus._setState({
+			savedData: {},
+			data: {},
+			loading: false,
+			initialLoaded: false,
+			changed: false,
+			error: false
+		});
+	}
+	componentDidMount() {
+		this.loadData();
+	}
+	reset() {
+		this.loadData();
 	}
 	render() {
 		const {bauhaus} = this.props;
-		if (this.state.error === true) {
+		if (bauhaus._state.error === true) {
 			return (
 				<div look={styles.center}><br/>{$('$core.content.error')}</div>
 			);
 		}
-		if (this.state.loading === true || this.state.initialLoaded === false) {
+		if (bauhaus._state.loading === true || bauhaus._state.initialLoaded === false) {
 			return (
 				<div look={styles.center}><br/><img src="media/loader.gif"/></div>
 			);
 		}
-      var saveColor = styles.gray;
-      if(this.state.changed === true){
-         saveColor = styles.green;
-      }
+		var saveColor = styles.gray;
+		if (bauhaus._state.changed === true) {
+			saveColor = styles.green;
+		}
 		return (
 			<div>
 				<span look={styles.contentHeadline}>{bauhaus.props.title}</span><hr look={styles.contentHr}/>
-				<input look={[styles.button, saveColor]} type="button" value="Speichern" key="b1"/>
-            <input look={[styles.button, styles.gray]} type="button" value="Zurücksetzen" key="b2"/>
-            <input look={[styles.button, styles.gray, styles.hoverRed]} type="button" value="Löschen" key="b3"/>
+				<input look={[styles.button, saveColor,]} type="button" value="Save" onClick={this
+					.saveData
+					.bind(this)} key="b1"/>
+				<input look={[styles.button, styles.gray,]} type="button" value="Reset" onClick={this
+					.reset
+					.bind(this)} key="b2"/>
+				<input look={[styles.button, styles.gray, styles.hoverRed,]} type="button" value="Delete" onClick={this
+					.delete
+					.bind(this)} key="b3"/>
 				<br/><br/>
 				{_
 					.map(bauhaus._childrenGenerators, function(component, key) {
