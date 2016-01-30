0f18ea1ab53909a5b0738c6cb105cfd215769779
dustin-H <dustin.hoffner@gmail.com>
2016-01-30 12:36:59 +0100

    JsonForm #1 now can load data display and change
    TODO: Save, Delete, Reset

--- src/modules/JsonForm/index.js
+++ src/modules/JsonForm/index.js
@@ -2,33 +2,89 @@ import React, {PropTypes, Component} from 'react';
 import Look, {StyleSheet} from 'react-look';
 import {$} from '../../utils/i18n/index.js';
 import _ from 'lodash';
+import objectPath from 'object-path';
+import superagent from 'superagent';
+import superagentPlugin from '../../utils/helpers/superagentPlugin.js';
 
 class JsonForm extends Component {
-   constructor(){
-      super();
-      this.state = {
-
-      }
-   }
-   setValue(path){
-      var path = 'test.look.seven';
-      var arr = path.split('.');
-      var dataObject = {};
-      var data = dataObject;
-      for(var i in arr){
-         var key = arr[i];
-         if(data)
-      }
-
-   }
-   render() {
+	constructor() {
+		super();
+		this.state = {
+			savedData: {},
+			data: {},
+			loading: false,
+			initialLoaded: false,
+			changed: false,
+			error: false
+		}
+	}
+	getValue(path) {
+		return objectPath.get(this.state.data, path);
+	}
+	setValue(path, value) {
+		var state = Object.assign({}, this.state);
+		if (state.initialLoaded === true && state.loading === false) {
+			var data = state.data;
+			objectPath.set(data, path, value);
+			state.data = data;
+			state.changed = JSON.stringify(state.data) === JSON.stringify(state.savedData);
+		}
+		this.setState(state);
+	}
+	componentDidMount() {
+		this.state.loading = true;
+		this.setState(this.state);
+		superagent
+			.get(this.props.bauhaus.props.getUrl)
+			.accept('json')
+			.use(superagentPlugin())
+			.end((function(err, res) {
+				if (err != null || res == null || res.body == null || res.body.jsondata == null) {
+					this.state.error = true;
+					return this.setState(this.state);
+				}
+				this.state.data = res.body.jsondata;
+				this.state.savedData = res.body.jsondata;
+				this.state.initialLoaded = true;
+				this.state.loading = false;
+				this.setState(this.state);
+			}).bind(this));
+	}
+	render() {
 		const {bauhaus} = this.props;
+		if (this.state.error === true) {
+			return (
+				<div look={styles.center}><br/>{$('$core.content.error')}</div>
+			);
+		}
+		if (this.state.loading === true || this.state.initialLoaded === false) {
+			return (
+				<div look={styles.center}><br/><img src="media/loader.gif"/></div>
+			);
+		}
+      var saveColor = styles.gray;
+      if(this.state.changed === true){
+         saveColor = styles.green;
+      }
 		return (
 			<div>
+				<span look={styles.contentHeadline}>{bauhaus.props.title}</span><hr look={styles.contentHr}/>
+				<input look={[styles.button, saveColor]} type="button" value="Speichern" key="b1"/>
+            <input look={[styles.button, styles.gray]} type="button" value="Zurücksetzen" key="b2"/>
+            <input look={[styles.button, styles.gray, styles.hoverRed]} type="button" value="Löschen" key="b3"/>
+				<br/><br/>
 				{_
 					.map(bauhaus._childrenGenerators, function(component, key) {
-						return component({some: 'props HAHA', key: key})
-					})}
+						return component({
+							get: this
+								.getValue
+								.bind(this),
+							set: this
+								.setValue
+								.bind(this),
+							key: key,
+						})
+					}.bind(this))}
 			</div>
 		);
 	}
--- src/modules/JsonForm/style.js
+++ src/modules/JsonForm/style.js
@@ -1,19 +1,54 @@
 var style = {
-	textInput: {
-		padding: '8px',
-		paddingLeft: '16px',
-		paddingRight: '16px',
-		borderRadius: '50px', // 4px
-		border: '1px solid rgb(210, 210, 210)',
-		boxSizing: 'border-box',
-		fooBar: 'none',
-		outline: 'none',
-		color: '#4E4E4E',
+	contentHeadline: {
+		fontWeight: 100,
+		color: '#676767',
+		fontSize: '32px'
+	},
+	contentHr: {
+		border: 'none',
+		height: '1px',
+		backgroundColor: 'rgba(0, 0, 0, 0.15)',
+		marginLeft: '-10px',
+		width: 'calc(100% + 20px)'
+	},
+	center: {
+		textAlign: 'center'
+	},
+	button: {
+		paddingTop: 9,
+		paddingBottom: 9,
+		paddingLeft: 16,
+		paddingRight: 16,
+		minWidth: '116px',
 		fontSize: '14px',
-		width: '100%',
-		':focus': {
-			border: '1px solid #20C753', // F96331 6E00FF
-			color: '#000000'
+		borderRadius: '38px',
+		border: 'none',
+		outline: 'none',
+		transition: 'ease-in-out 0.1s',
+		cursor: 'pointer',
+		marginTop: 10,
+		marginRight: 10,
+		':hover': {
+			//opacity: 0.6,
+			transform: 'scaleX(1.1) scaleY(1.1)'
+		}
+	},
+	green: {
+		backgroundColor: 'rgba(32, 199, 83, 1)',
+		color: '#ffffff'
+	},
+	red: {
+		backgroundColor: 'rgb(255, 59, 0)',
+		color: '#ffffff'
+	},
+	gray: {
+		backgroundColor: 'rgb(234, 234, 234)',
+		color: '#000000'
+	},
+	hoverRed: {
+		':hover': {
+			backgroundColor: 'rgb(255, 59, 0)',
+			color: '#ffffff'
 		}
 	}
 };
