bbf93d7bad8796ab0ec9f4a7f97f1c293c90c681
dustin-H <dustin.hoffner@gmail.com>
2016-01-30 16:15:22 +0100

    Add es6shim. Closes #42

--- uglify.js
+++ uglify.js
@@ -4,7 +4,7 @@ console.log('Minify...');
 var result = UglifyJS.minify('build/bundle.js');
 console.log('Saving...');
 
-//var browserFixes = fs.readFileSync('es6shim.js');
+var browserFixes = fs.readFileSync('es6shim.js');
 
-fs.writeFileSync('build/bundle.js', result.code);
+fs.writeFileSync('build/bundle.js', browserFixes+result.code);
 console.log('FINISH');
