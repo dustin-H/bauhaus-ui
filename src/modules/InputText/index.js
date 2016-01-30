35dc18b70c417625de0f4f1db5007fe52aaedeb1
dustin-H <dustin.hoffner@gmail.com>
2016-01-30 12:37:28 +0100

    Changed InputText #3 to work with JsonForm #1

--- src/modules/InputText/index.js
+++ src/modules/InputText/index.js
@@ -1,17 +1,19 @@
-import React, {PropTypes, Component} from 'react';
+import React, {PropTypes, Component,} from 'react';
 import Look, {StyleSheet} from 'react-look';
 import {$} from '../../utils/i18n/index.js';
 
 class InputText extends Component {
+	handleChange(event) {
+		const {bauhaus, get, set,} = this.props;
+		var value = event.target.value;
+		set(bauhaus.props.path, value);
+	}
 	render() {
-		const {bauhaus, some} = this.props;
+		const {bauhaus, get, set,} = this.props;
 		return (
-			<span>
-				<span>{$(bauhaus.props.label)}
-					+
-					{some}</span>
-				<input look={styles.textInput} type="text" defaultValue={$(bauhaus.props.defaultValue)}></input>
-			</span>
+			<input look={styles.textInput} type="text" value={get(bauhaus.props.path)} onChange={this
+				.handleChange
+				.bind(this)}></input>
 		);
 	}
 }
