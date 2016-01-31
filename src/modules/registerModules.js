17e33843db04c746f39ad1923d5c892d0ddbc793
dustin-H <dustin.hoffner@gmail.com>
2016-01-31 13:32:19 +0100

    Register InputPassword
    See #9

--- src/modules/registerModules.js
+++ src/modules/registerModules.js
@@ -8,12 +8,14 @@ import InputText from './InputText';
 import Label from './Label';
 import JsonForm from './JsonForm';
 import Section from './Section';
+import InputPassword from './InputPassword';
 
 var registerModules = function() {
 	registerModule('InputText', InputText);
 	registerModule('Label', Label);
 	registerModule('JsonForm', JsonForm);
 	registerModule('Section', Section);
+	registerModule('InputPassword', InputPassword);
 }
 
 export default registerModules;
