2a4d5e446a91ce8548738f9f4cc6c9d2d19827e4
dustin-H <dustin.hoffner@gmail.com>
2016-01-31 13:37:34 +0100

    Register InputEmail
    See #8

--- src/modules/registerModules.js
+++ src/modules/registerModules.js
@@ -9,6 +9,7 @@ import Label from './Label';
 import JsonForm from './JsonForm';
 import Section from './Section';
 import InputPassword from './InputPassword';
+import InputEmail from './InputEmail';
 
 var registerModules = function() {
 	registerModule('InputText', InputText);
@@ -16,6 +17,7 @@ var registerModules = function() {
 	registerModule('JsonForm', JsonForm);
 	registerModule('Section', Section);
 	registerModule('InputPassword', InputPassword);
+	registerModule('InputEmail', InputEmail);
 }
 
 export default registerModules;
