35b6834b4c14bb14ffe28c61c22dc18ae375bc7d
dustin-H <dustin.hoffner@gmail.com>
2016-02-04 00:15:09 +0100

    DevTest for JsonForm Validation #48

--- src/modules/InputText/index.js
+++ src/modules/InputText/index.js
@@ -1,4 +1,4 @@
-import React, {PropTypes, Component} from 'react';
+import React, {PropTypes, Component,} from 'react';
 import Look, {StyleSheet} from 'react-look';
 import {$} from '../../utils/i18n/index.js';
 
@@ -6,14 +6,43 @@ class InputText extends Component {
 	handleChange(event) {
 		const {bauhaus, get, set,} = this.props;
 		var value = event.target.value;
-		set(bauhaus.props.path, value);
+		set(bauhaus.props.path, value, this.validate(value));
+	}
+	validate(value) {
+		if (value == null) {
+			return '';
+		}
+		if (value === 'böse') {
+			return 'Not allowed!';
+		}
+		return true;
+	}
+	componentDidMount() {
+		const {bauhaus, get, set, setValidator,} = this.props;
+		setValidator(bauhaus.props.path, this.validate)
 	}
 	render() {
-		const {bauhaus, get, set,} = this.props;
+		const {bauhaus, get, set, isValid,} = this.props;
+		var valid = isValid(bauhaus.props.path);
+		var invalidError;
+		if (valid === true) {
+			invalidError = (
+				<span></span>
+			);
+		} else {
+			invalidError = (
+				<span><br/>
+					<span>{valid}</span>
+				</span>
+			)
+		}
 		return (
-			<input look={styles.textInput} type="text" value={get(bauhaus.props.path)} onChange={this
-				.handleChange
-				.bind(this)}></input>
+			<span>
+				<input look={styles.textInput} type="text" value={get(bauhaus.props.path)} onChange={this
+					.handleChange
+					.bind(this)}></input>
+				{invalidError}
+			</span>
 		);
 	}
 }
