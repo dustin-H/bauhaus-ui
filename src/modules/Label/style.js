4259074a44c8e83ce6b66da20b0f82564eda5a7b
dustin-H <dustin.hoffner@gmail.com>
2016-02-13 15:48:10 +0100

    Clean up!

--- src/modules/Label/style.js
+++ src/modules/Label/style.js
@@ -5,32 +5,37 @@ var style = {
 		fontSize: 16,
 		fontWeight: 500
 	},
-   labelWrapper: {
-   },
-   info: {
-      fontSize: 8,
-      fontWeight: 700,
-      cursor: 'pointer',
-      borderRadius: 4,
-      backgroundColor: '#F1BD00',
-      padding: 2,
-      paddingLeft: 4,
-      paddingRight: 4,
-      position: 'relative',
-      top: -2
-   },
-   infobox: {
-      fontSize: 14,
-      fontWeight: 500,
-      cursor: 'pointer',
-      borderRadius: 18,
-      backgroundColor: '#F1BD00',
-      padding: 8,
-      paddingLeft: 16,
-      paddingRight: 16,
+	labelWrapper: {},
+	info: {
+		fontSize: 8,
+		fontWeight: 700,
+		cursor: 'pointer',
+		borderRadius: 4,
+		backgroundColor: '#F1BD00',
+		padding: 2,
+		paddingLeft: 4,
+		paddingRight: 4,
+		position: 'relative',
+		top: -2,
+		userSelect: 'none',
+		transition: 'ease-in-out 0.1s',
+		':hover': {
+			paddingLeft: 8,
+			paddingRight: 8
+		}
+	},
+	infobox: {
+		fontSize: 14,
+		fontWeight: 500,
+		cursor: 'pointer',
+		borderRadius: 18,
+		backgroundColor: '#F1BD00',
+		padding: 8,
+		paddingLeft: 16,
+		paddingRight: 16,
 		border: '1px solid #F1BD00',
-      position: 'relative',
-      marginBottom: 8
-   }
+		position: 'relative',
+		marginBottom: 8
+	}
 };
 export default style;
