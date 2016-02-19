8061c289e4ed5a5f34599ea507d346047842d201
dustin-H <dustin.hoffner@gmail.com>
2016-02-19 23:23:43 +0100

    Build process for core modules
    #73

--- uglify.js
+++ uglify.js
@@ -1,12 +1,26 @@
 var UglifyJS = require('uglify-js');
 var fs = require('fs');
-console.log('Minify...');
-var result = UglifyJS.minify('build/bundle.js');
-console.log('Saving...');
+console.log('> Minify main bundle...');
+var result = UglifyJS.minify('public/bundle.js');
 
 var browserFixes = fs.readFileSync('es6shim.js');
 
-//var prod = "var process={env:{NODE_ENV:'production'}};var NODE_ENV='production';";
+// var prod = "var process={env:{NODE_ENV:'production'}};var NODE_ENV='production';";
 
-fs.writeFileSync('build/bundle.js', browserFixes+result.code);
+fs.writeFileSync('public/bundle.js', browserFixes + result.code);
+console.log('>   Done!');
+
+console.log('Minify core modules...');
+
+var modules = require('./src/coreModules.js');
+
+for (var i in modules) {
+  var name = modules[i];
+  var path = 'public/modules/' + name + '.js';
+  console.log('> Minify ' + path);
+  var result = UglifyJS.minify(path);
+  fs.writeFileSync(path, result.code);
+  console.log('>   Done!');
+}
+
 console.log('FINISH');
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,78 @@
-var webpack = require('webpack');
+var webpack = require('webpack')
+var fs = require('fs')
 
-module.exports = [{
-		name: 'bauhaus-ui',
-		context: __dirname + "/",
-		entry: __dirname + "/src/index.js",
-		output: {
-			filename: "bundle.js",
-			path: __dirname + "/public/",
-		},
-		module: {
-			loaders: [{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: "babel-loader"
-			}]
-		}
-	}]
-	/*
-	,{
-	   name: 'bauhaus-ui-module-InputText',
-		context: __dirname + "/",
-		entry: __dirname + "/modules/InputText/src/index.js",
+var modulesPath = 'modules'
 
-		output: {
-			filename: "bundle.js",
-			path: __dirname + "/modules/InputText/build/",
-		},
+var modules = fs.readdirSync(__dirname + '/' + modulesPath)
+if (modules.indexOf('.DS_Store') >= 0) {
+  modules.splice(modules.indexOf('.DS_Store'), 1)
+}
 
-		module: {
-			loaders: [{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: "babel-loader"
-			}]
-		}
-	} */
+var config = [{
+  name: 'bauhaus-ui',
+  context: __dirname + '/',
+  entry: __dirname + '/src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/public/'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
+  }
+}]
+
+for (var i in modules) {
+  var name = modules[i]
+  config.push({
+    name: 'bauhaus-ui-' + name,
+    context: __dirname + '/',
+    entry: __dirname + '/' + modulesPath + '/' + name + '/index.js',
+    output: {
+      filename: name + '.js',
+      path: __dirname + '/public/modules/'
+    },
+    resolve: {
+      alias: {
+        react: 'bauhaus-ui-module-utils/npm/react',
+        'react-dom': 'bauhaus-ui-module-utils/npm/react-dom',
+        'react-look': 'bauhaus-ui-module-utils/npm/react-look',
+        superagent: 'bauhaus-ui-module-utils/npm/superagent',
+        lodash: 'bauhaus-ui-module-utils/npm/lodash'
+      }
+    },
+    module: {
+      loaders: [{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }]
+    }
+  })
+}
+
+fs.writeFileSync(__dirname + '/src/coreModules.js', 'module.exports = '+JSON.stringify(modules))
+
+module.exports = config;
+/*
+,{
+   name: 'bauhaus-ui-module-InputText',
+	context: __dirname + "/",
+	entry: __dirname + "/modules/InputText/src/index.js",
+
+	output: {
+		filename: "bundle.js",
+		path: __dirname + "/modules/InputText/build/",
+	},
+
+	module: {
+		loaders: [{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			loader: "babel-loader"
+		}]
+	}
+} */
