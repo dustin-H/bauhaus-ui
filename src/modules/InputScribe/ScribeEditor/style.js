3284755907c269a9f326a74bb9a2ac577ea8749a
dustin-H <dustin.hoffner@gmail.com>
2016-02-21 20:47:29 +0100

    Removed hard imported modules
    For #73

--- src/modules/InputScribe/ScribeEditor/style.js
+++
@@ -1,4 +0,0 @@
-var style = {
-	
-};
-export default style;
--- src/modules/Validator/style.js
+++
@@ -1,9 +0,0 @@
-var style = {
-   error: {
-      color: '#ff0000',
-      fontWeight: 700,
-		fontSize: 11,
-      paddingLeft: 17
-   }
-};
-export default style;
