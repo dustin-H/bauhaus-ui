eec9190344e23bd3ab08292ea29fa081f1f84c76
dustin-H <dustin.hoffner@gmail.com>
2016-02-22 00:01:02 +0100

    SimpleLinkList with filter now working
    Closes #66

--- modules/SimpleLinkList/index.js
+++ modules/SimpleLinkList/index.js
@@ -1,24 +1,30 @@
-import React, { PropTypes, Component } from 'react';
-import Look, { StyleSheet } from 'react-look';
-const c = StyleSheet.combineStyles;
-import { $ } from 'bauhaus-ui-module-utils';
-import _ from 'lodash';
+import React, { PropTypes, Component } from 'react'
+import Look, { StyleSheet } from 'react-look'
+const c = StyleSheet.combineStyles
+import { $, pushLocation } from 'bauhaus-ui-module-utils'
+import _ from 'lodash'
 
 class SimpleLinkList extends Component {
   constructor(props) {
     super(props);
     this.handleFilterChange = this
       .handleFilterChange
-      .bind(this);
+      .bind(this)
   }
   handleFilterChange(event) {
-    const {bauhaus} = this.props;
+    const {bauhaus} = this.props
     bauhaus._setState({
       filter: event.target.value
     });
   }
-  componentWillMount() {
+  pushLocation(location) {
     const {bauhaus} = this.props;
+    return function() {
+      pushLocation(location)
+    }
+  }
+  componentWillMount() {
+    const {bauhaus} = this.props
     bauhaus._setState({
       filter: ''
     });
@@ -32,22 +38,23 @@ class SimpleLinkList extends Component {
         <hr className={ styles.contentHr } />
         <br/>
         <span>Filter: <input className={ styles.textInput } type="text" value={ bauhaus._state.filter } onChange={ this.handleFilterChange } /></span>
-        <br/><br/>
+        <br/>
+        <br/>
         { _.map(bauhaus.props.list, function(value, key) {
             if (value.name.search(bauhaus._state.filter) >= 0) {
-              return (<div key={key} className={styles.listElement}>
+              return (<div key={ key } className={ styles.listElement } onClick={ this.pushLocation({pathname: value.pathname}) }>
                         { $(value.name) }
                       </div>)
             } else {
-              return ''
+              return <span></span>
             }
-          }) }
+          }.bind(this)) }
       </div>
     )
   }
 }
 
-import styleSheet from './style.js';
-var styles = StyleSheet.create(styleSheet);
+import styleSheet from './style.js'
+var styles = StyleSheet.create(styleSheet)
 
-__GLOBAL__.exportDefault = Look(SimpleLinkList);
+__GLOBAL__.exportDefault = Look(SimpleLinkList)
