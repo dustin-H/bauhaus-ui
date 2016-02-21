32c7719e0f6743f77ebc3a762e7e1c1b7ed3d4be
dustin-H <dustin.hoffner@gmail.com>
2016-02-21 22:31:33 +0100

    Push location for modules

--- src/utils/moduleLoader/api.js
+++ src/utils/moduleLoader/api.js
@@ -1,15 +1,19 @@
 
+import {pushLocation} from '../../actions/router.js'
+import store from '../../store/store.js'
 
 __GLOBAL__.npm = {
-	react: require('react'),
-	'react-dom': require('react-dom'),
-	superagent: require('superagent'),
-	lodash: require('lodash'),
-	'react-look': require('react-look')
+  react: require('react'),
+  'react-dom': require('react-dom'),
+  superagent: require('superagent'),
+  lodash: require('lodash'),
+  'react-look': require('react-look')
 };
 
 __GLOBAL__.bauhaus = {
-	superagentPlugin: require('../helpers/superagentPlugin.js'),
-	$: require('../i18n/index.js')
-		.$
+  superagentPlugin: require('../helpers/superagentPlugin.js'),
+  $: require('../i18n/index.js').$
+  pushLocation: function(location){
+    store.dispatch(pushLocation(location))
+  }
 }
