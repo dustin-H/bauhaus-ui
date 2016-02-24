7979498dc3cb3cca57c829f00f9a5434910c436a
dustin-H <dustin.hoffner@gmail.com>
2016-02-24 11:42:59 +0100

    Icons for draft-js editor

---
+++ modules/InputDraft/DraftEditor/icons.js
@@ -0,0 +1,27 @@
+import bold from './icons/bold.svg'
+import italic from './icons/italic.svg'
+import underline from './icons/underline.svg'
+import outdent from './icons/indent-decrease.svg'
+import indent from './icons/indent-increase.svg'
+import list from './icons/list.svg'
+import listNumbered from './icons/list-numbered.svg'
+import strikethrough from './icons/strikethrough.svg'
+import subscript from './icons/subscript.svg'
+import superscript from './icons/superscript.svg'
+import undo from './icons/undo2.svg'
+import redo from './icons/redo2.svg'
+
+export default {
+  bold,
+  italic,
+  underline,
+  outdent,
+  indent,
+  list,
+  listNumbered,
+  strikethrough,
+  subscript,
+  superscript,
+  undo,
+  redo
+}
