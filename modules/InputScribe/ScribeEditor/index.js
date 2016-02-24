ca07a98694edce06f54718ed4a5fd8f14af50322
dustin-H <dustin.hoffner@gmail.com>
2016-02-24 11:37:41 +0100

    InputScribe redesign
    For #78

--- modules/InputScribe/ScribeEditor/index.js
+++ modules/InputScribe/ScribeEditor/index.js
@@ -3,6 +3,8 @@ import Look, { StyleSheet } from 'react-look'
 const c = StyleSheet.combineStyles
 import Scribe from 'scribe-editor'
 import scribePluginToolbar from 'scribe-plugin-toolbar'
+//import Icon from 'babel!svg-react!./icons/bin.svg?name=Icon'
+import icons from './icons.js'
 
 class ScribeEditor extends Component {
   constructor() {
@@ -53,18 +55,18 @@ class ScribeEditor extends Component {
         <div ref={ this
                      .setRef('toolbar')
                      .bind(this) }>
-          <button data-command-name="bold">Bold</button>
-          <button data-command-name="italic">Italic</button>
-          <button data-command-name="underline">Underline</button>
-          <button data-command-name="strikeThrough">Strike Through</button>
-          <button data-command-name="indent">Indent</button>
-          <button data-command-name="outdent">Outdent</button>
-          <button data-command-name="undo">Undo</button>
-          <button data-command-name="redo">Redo</button>
-          <button data-command-name="subscript">Subscript</button>
-          <button data-command-name="subscript">Superscript</button>
-          <button data-command-name="insertOrderedList">Ordered List</button>
-          <button data-command-name="insertUnorderedList">Unordered List</button>
+          <button className={styles.button} data-command-name="bold"><icons.bold className={styles.icon}/></button>
+          <button className={styles.button} data-command-name="italic"><icons.italic className={styles.icon}/></button>
+          <button className={styles.button} data-command-name="underline"><icons.underline className={styles.icon}/></button>
+          <button className={styles.button} data-command-name="strikeThrough"><icons.strikethrough className={styles.icon}/></button>
+          <button className={styles.button} data-command-name="indent"><icons.indent className={styles.icon}/></button>
+          <button className={styles.button} data-command-name="outdent"><icons.outdent className={styles.icon}/></button>
+          <button className={styles.button} data-command-name="undo"><icons.undo className={styles.icon}/></button>
+          <button className={styles.button} data-command-name="redo"><icons.redo className={styles.icon}/></button>
+          <button className={styles.button} data-command-name="subscript"><icons.subscript className={styles.icon}/></button>
+          <button className={styles.button} data-command-name="superscript"><icons.superscript className={styles.icon}/></button>
+          <button className={styles.button} data-command-name="insertOrderedList"><icons.listNumbered className={styles.icon}/></button>
+          <button className={styles.button} data-command-name="insertUnorderedList"><icons.list className={styles.icon}/></button>
         </div>
         <div className={ this.props.className } ref={ this
                                                         .setRef('main')
--- modules/InputScribe/ScribeEditor/style.js
+++ modules/InputScribe/ScribeEditor/style.js
@@ -1,2 +1,26 @@
-var style = {}
+var style = {
+  button: {
+    position: 'relative',
+    backgroundColor: '#ffffff',
+    cursor: 'pointer',
+    opacity: 0.7,
+    ':disabled': {
+      opacity: '0.3 !important',
+      cursor: 'not-allowed'
+    },
+    ':hover': {
+      opacity: 1
+    },
+    '.active': {
+      backgroundColor: '#20C753'
+    },
+    padding: 10,
+    border: 'none',
+    outline: 'none'
+  },
+  icon: {
+    width: 18,
+    height: 18
+  }
+}
 export default style
