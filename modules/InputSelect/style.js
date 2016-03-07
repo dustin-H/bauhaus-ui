d08cf991833204206430467836e5c477e7083bc1
dustin-H <dustin.hoffner@gmail.com>
2016-03-07 12:05:44 +0100

    InputSelect: New option multiple
    Closes #70

--- modules/InputSelect/style.js
+++ modules/InputSelect/style.js
@@ -11,7 +11,7 @@ var style = {
     color: '#4E4E4E',
     fontSize: 14,
     width: '100%',
-    height: 35,
+    height: 37,
     ':focus': {
       border: '1px solid #20C753', // F96331 6E00FF
       color: '#000000'
@@ -27,6 +27,74 @@ var style = {
       color: '#ff0000',
       backgroundColor: 'transparent'
     }
+  },
+  box: {
+    display: 'flex',
+    flexWrap: 'nowrap',
+    justifyContent: 'space-between',
+    marginBottom: 10
+  },
+  innerBox: {
+    display: 'flex',
+    flex: 2,
+    maxHeight: 37
+  },
+  spaceBox: {
+    display: 'flex',
+    width: 10
+  },
+  buttonBox: {
+    display: 'flex',
+    maxHeight: 37,
+    width: 116
+  },
+  button: {
+    padding: 8,
+    paddingLeft: 16,
+    paddingRight: 16,
+    minWidth: '116px',
+    fontSize: '14px',
+    borderRadius: 5, // '38px'
+    border: 'none',
+    outline: 'none',
+    maxHeight: 37,
+    transition: 'ease-in-out 0.1s',
+    cursor: 'pointer',
+    backgroundColor: 'rgb(234, 234, 234)',
+    color: '#000000',
+    ':hover': {
+      // opacity: 0.6,
+      transform: 'scaleX(1.1) scaleY(1.1)'
+    }
+  },
+  selected: {
+    display: 'inline-block'
+  },
+  selectedValue: {
+    display: 'inline-block',
+    padding: 8,
+    paddingLeft: 16,
+    paddingRight: 16,
+    minWidth: '116px',
+    fontSize: '14px',
+    borderRadius: 5, // '38px'
+    border: 'none',
+    maxHeight: 37,
+    marginRight: 10,
+    marginBottom: 10,
+    cursor: 'pointer',
+    textAlign: 'center',
+    backgroundColor: 'rgb(234, 234, 234)',
+    transition: 'ease-in-out 0.1s',
+    color: '#000000',
+    ':hover': {
+      backgroundColor: 'rgb(255, 59, 0)',
+      color: '#ffffff',
+      transform: 'scaleX(1.1) scaleY(1.1)'
+    }
+  },
+  hidden: {
+    display: 'none'
   }
 }
 export default style
