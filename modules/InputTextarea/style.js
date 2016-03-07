5edffba4be5e466a9cd3fd7d1aea74ea1ff0bb04
dustin-H <dustin.hoffner@gmail.com>
2016-03-07 23:43:46 +0100

    Textarea resizing fix

--- modules/InputTextarea/style.js
+++ modules/InputTextarea/style.js
@@ -10,13 +10,13 @@ var style = {
     outline: 'none',
     color: '#4E4E4E',
     fontSize: '14px',
-    width: '100%',
-    maxWidth: '100%',
+    width: '100% !important',
+    maxWidth: '100% !important',
     ':focus': {
       border: '1px solid #20C753', // F96331 6E00FF
       color: '#000000'
     },
-    marginBottom: 10
+    marginBottom: 6
   },
   inputError: {
     color: '#ff0000',
