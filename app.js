f85eadc8dcdd0212c92450ac6048f478bcd67ce1
dustin-H <dustin.hoffner@gmail.com>
2016-03-10 14:52:31 +0100

    Proxy to strongloop for testing InputFiles
    For #12

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ var express = require('express');
 var testapi = require('./testapi/app.js');
 var bauhausui = require('./index.js');
 var config = require('./config.json');
+var proxy = require('express-http-proxy');
 
 var app = express();
 
 app.use(function(req, res, next) {
-	//res.header('Access-Control-Allow-Origin', '*');
-	next();
+  //res.header('Access-Control-Allow-Origin', '*');
+  next();
 });
 
 /*app.use('/config.json', function(req, res, next) {
@@ -20,8 +21,14 @@ app.use('/api', testapi())
 
 app.use(bauhausui(config));
 
+app.use(proxy('localhost:3000', {
+  forwardPath: function(req, res) {
+    return require('url').parse(req.url).path;
+  }
+}));
+
 var port = 1234;
 
-app.listen(port, function(){
-   console.log('Listening to port', port);
+app.listen(port, function() {
+  console.log('Listening to port', port);
 });
