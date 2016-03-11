b0d5626212b4203d4f887660aaa1486ac6bd5c99
dustin-H <dustin.hoffner@gmail.com>
2016-03-11 12:37:52 +0100

    Fix Material UI Design Bug

--- modules/InputDateTime/MaterialUiDate/index.js
+++ modules/InputDateTime/MaterialUiDate/index.js
@@ -11,12 +11,35 @@ class MaterialUiDate extends Component {
     const {value, valid, onChange, hint} = this.props
     return (
       <DatePicker hintText={ hint } value={ value } onChange={ this.handleChange.bind(this) } underlineShow={ false } fullWidth={ true }
-        style={ {  width: '100%'} } hintStyle={ {  bottom: '16px',  left: '16px'} } />
+        style={ {  width: '100%'} } hintStyle={ {  bottom: '16px',  left: '16px'} } inputStyle = {
+  inputStyles
+} />
     )
   }
 }
 
-var styles = {
+const inputStyles = {
+  padding: '8px !important',
+  paddingLeft: '16px !important',
+  paddingRight: '16px !important',
+  borderRadius: '5px !important', // 4px
+  border: '1px solid rgb(210, 210, 210) !important',
+  boxSizing: 'border-box !important',
+  backgroundColor: 'transparent !important',
+  outline: 'none !important',
+  color: '#4E4E4E !important',
+  font: 'initial !important',
+  fontSize: '14px !important',
+  fontFamily: 'Open Sans !important',
+  width: '100% !important',
+  height: 'auto !important',
+  ':focus': {
+    border: '1px solid #20C753 !important', // F96331 6E00FF
+    color: '#000000 !important'
+  }
+}
+
+/*var styles = {
   'input[id|="mui"][type="text"]': {
     padding: '8px !important',
     paddingLeft: '16px !important',
@@ -39,6 +62,6 @@ var styles = {
   }
 }
 
-StyleSheet.addCSS(styles)
+StyleSheet.addCSS(styles)*/
 
 export default MaterialUiDate
--- modules/InputDateTime/MaterialUiTime/index.js
+++ modules/InputDateTime/MaterialUiTime/index.js
@@ -17,8 +17,29 @@ class MaterialUiTime extends Component {
     const {value, valid, onChange, hint} = this.props
     return (
       <TimePicker hintText={ hint } ref="picker" onChange={ this.handleChange.bind(this) } underlineShow={ false } fullWidth={ true } style={ {  width: '100%'} }
-        hintStyle={ {  bottom: '16px',  left: '16px'} } format="24hr" />
+        hintStyle={ {  bottom: '16px',  left: '16px'} } format="24hr" inputStyle={ inputStyles } />
     )
+  }
+}
+
+const inputStyles = {
+  padding: '8px !important',
+  paddingLeft: '16px !important',
+  paddingRight: '16px !important',
+  borderRadius: '5px !important', // 4px
+  border: '1px solid rgb(210, 210, 210) !important',
+  boxSizing: 'border-box !important',
+  backgroundColor: 'transparent !important',
+  outline: 'none !important',
+  color: '#4E4E4E !important',
+  font: 'initial !important',
+  fontSize: '14px !important',
+  fontFamily: 'Open Sans !important',
+  width: '100% !important',
+  height: 'auto !important',
+  ':focus': {
+    border: '1px solid #20C753 !important', // F96331 6E00FF
+    color: '#000000 !important'
   }
 }
 
