0bd230dd42463301eb1793b7dbe4717a062c7c4f
dustin-H <dustin.hoffner@gmail.com>
2016-03-29 20:37:14 +0200

    Test proxy to strongloop for inputFiles Test
    For #93

--- testapi.js
+++ testapi.js
@@ -2,6 +2,7 @@ var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
 var jsonParser = bodyParser.json()
+var proxy = require('http-proxy-middleware');
 
 
 var bauhausui = require('./index.js')
@@ -16,5 +17,11 @@ app.use('/static', express.static('./example/static'))
 app.use(bauhausui(config))
 
 app.post('/api/login', jsonParser, login)
+
+app.use(proxy('/', {
+  target: 'http://localhost:3000',
+  changeOrigin: false,
+  ws: false,
+}));
 
 app.listen(5000)
